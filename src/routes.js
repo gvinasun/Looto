dd37946e18e19c968fe8a31cd53736e770f812b5
Gina <gvinasun@uoc.edu>
2025-05-30 17:12:47 +0200

    Proyecto Looto2

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ const AppRoutes = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
